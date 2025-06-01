6d5e764bd957e607c9123d17324be13db550e79a
SoggyWaffles <designsbysoggy@gmail.com>
2025-06-01 20:48:51 +0800

    remove use client

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import CategoryPage from './main'
 import Header from '../components/header'
-import { Suspense } from 'react';
+
 
 const page = () => {
   return (
     <div className=''>
             <Header/>
             <div className='mt-30'>
-                      <Suspense fallback={<div className="text-white p-8">Loading...</div>}>
-      <CategoryPage />
-    </Suspense>
+                     <CategoryPage/>  
             </div>
             
     </div>
