bbdbaea7b39229742b520ccaed43b38681450885
SoggyWaffles <designsbysoggy@gmail.com>
2025-06-01 21:03:56 +0800

    sdfdsf

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,23 +1,19 @@
-// app/category/page.tsx
-import React from 'react';
+
+
 import CategoryPage from './main';
 import Header from '../components/header';
 
-interface PageProps {
-  searchParams: {
-    type?: string;
-  };
-}
-
-const Page = ({ searchParams }: PageProps) => {
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: { type?: string };
+}) {
   return (
-    <div className="">
+    <div>
       <Header />
       <div className="mt-30">
-        <CategoryPage initialType={searchParams.type} />
+        <CategoryPage initialType={searchParams?.type} />
       </div>
     </div>
   );
-};
-
-export default Page;
+}
